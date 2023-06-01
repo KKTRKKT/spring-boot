cb4dc568d19de0a5f54a3a6870040c31dff1417d
KKTRKKT <paulhana6006@gmail.com>
2023-06-01 10:19:46 +0900

    정적 리소스 설정 예제 추가

---
+++ src/main/java/me/kktrkkt/springboot/mvc/config/WebConfig.java
@@ -0,0 +1,17 @@
+package me.kktrkkt.springboot.mvc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    // 정적 리소스 매핑 커스터마이징, properties 설정보다 추천
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/mvc/static/**") // 리소스를 핸들할 url 설정
+                .addResourceLocations("classpath:/mvc/static/")  // 리소스가 존재하는 경로
+                .setCachePeriod(20); // 캐싱 유지 시간(초)
+    }
+}
