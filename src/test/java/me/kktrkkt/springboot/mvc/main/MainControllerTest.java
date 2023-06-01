274028b7851e177aa32b6587ff3ea73d5f3c783e
KKTRKKT <paulhana6006@gmail.com>
2023-06-01 14:20:25 +0900

    htmlunit 적용 예제 추가

--- src/test/java/me/kktrkkt/springboot/mvc/main/MainControllerTest.java
+++ src/test/java/me/kktrkkt/springboot/mvc/main/MainControllerTest.java
@@ -1,11 +1,17 @@
 package me.kktrkkt.springboot.mvc.main;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.io.IOException;
+
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -16,6 +22,9 @@ class MainControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private WebClient webClient;
+
     @Test
     public void main() throws Exception {
         this.mockMvc.perform(get("/"))
@@ -24,5 +33,16 @@ class MainControllerTest {
                 .andExpect(model().attribute("name", "kktrkkt"))
                 .andExpect(content().string(containsString("kktrkkt")))
                 .andDo(print());
+    }
+
+    @Test
+    public void main_html() throws IOException {
+        HtmlPage page = webClient.getPage("/");
+        HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+        assertTrue(h1.getTextContent().equalsIgnoreCase("kktrkkt"));
+//        DomNodeList<HtmlElement> h1 = page.getBody().getElementsByTagName("h1");
+//        assertNotNull(h1.get(0));
+//        assertTrue(h1.get(0).getTextContent().equalsIgnoreCase("kktrkkt"));
+
     }
 }
