2b2d36171833a294b0ea2b499ab4b3eb66bdb1a0
KKTRKKT <paulhana6006@gmail.com>
2023-06-02 10:55:11 +0900

    hikari 프로퍼티 추가

--- src/main/java/me/kktrkkt/springboot/data/h2/Application.java
+++ src/main/java/me/kktrkkt/springboot/data/h2/Application.java
@@ -2,8 +2,10 @@ package me.kktrkkt.springboot.data.h2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/data/h2/application.properties")
 public class Application {
 
     public static void main(String[] args) {
